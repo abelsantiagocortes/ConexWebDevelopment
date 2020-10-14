fada0fee2fa12cf9dbd4db5dc3ead918cd466614
Juan Sebastian <seposorio@hotmail.com>
2020-10-14 12:58:18 -0500

    Cards done --> Problem with banner

--- src/components/Terapias.js
+++ src/components/Terapias.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import styled from 'styled-components/macro';
+import '../css/Terapias.css';
+import TerapiasCard from '../components/TerapiasCard'
 const TerapiasContainer = styled.div`
     {
-       
+        
         h1{
-            font-family: 'Lobster', cursive; 
+            margin-left: 60px;
+            margin-top: 40px;
+            font-family: 'Lobster',cursive;
+            color: white;
         }
 
     }`
 
 function Terapias() {
     return (
-        <TerapiasContainer>
-            <h1>Terapias</h1>
+        <TerapiasContainer className="terapias">
+            <div className="terapias__bannerContainer">
+                <h1>Mis Terapias</h1>
+                <img className="terapias__banner" src="https://i.ibb.co/rbFZC6b/Imagen-1.png" alt="Imagen-1" border="0"></img>
+            </div>
+            <div className="terapias__cards">
+                <TerapiasCard/>
+                <TerapiasCard/>
+
+                <TerapiasCard/>
+
+                <TerapiasCard/>
+                <TerapiasCard/>
+
+            </div>
         </TerapiasContainer>
     )
 }
