aaed1d7e115c3664e6d6f10ac4abca2b01555ab4
Abel Cortes <abelsantiagocortes@gmail.com>
2020-12-11 13:57:07 +1100

    Updates

---
+++ precache-manifest.2b733dfa850c261efbd55659420ffb0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9472a233d89de632d2384a9312737b",
+    "url": "/ConexWebDevelopment/index.html"
+  },
+  {
+    "revision": "c11f9e2827cc21b3c525",
+    "url": "/ConexWebDevelopment/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "99d83ba87e005e0cb17e",
+    "url": "/ConexWebDevelopment/static/css/main.1c0b2a36.chunk.css"
+  },
+  {
+    "revision": "c11f9e2827cc21b3c525",
+    "url": "/ConexWebDevelopment/static/js/2.c9f8dea4.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/ConexWebDevelopment/static/js/2.c9f8dea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d83ba87e005e0cb17e",
+    "url": "/ConexWebDevelopment/static/js/main.9dbabd12.chunk.js"
+  },
+  {
+    "revision": "96b66bee4e1c1e6dc86f",
+    "url": "/ConexWebDevelopment/static/js/runtime-main.24fda760.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/ConexWebDevelopment/static/media/getFetch.013f0b90.cjs"
+  }
+]);
