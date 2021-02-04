21f1af75ea6380a2c335390de8752aa2a6160c87
Juan Sebastián Osorio García <seposorio@hotmail.com>
2021-02-03 21:59:53 -0500

    New tab when click on Social and Footer component - Update dates on blog

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,10 +48,11 @@ function Footer() {
             </div>
 
             <div className="footer__socialLinks">
-                <a href="https://wa.me/5215529018021?text=Bienvenido%20a%20Conexión%20y%20Consciencia,%20Terapia%20con%20Ángeles,%20en%20que%20puedo%20ayudarte?" className="whatsapp"><WaImg alt="" src='https://i.ibb.co/7g13zC5/whatsapp.png' /></a>
-                <a href="https://www.facebook.com/conexionyconsciencia" className="facebook"><img alt="" src='https://i.ibb.co/MNMxD7M/facebook.png' /></a>
-                <a href="https://www.youtube.com/channel/UCFLEejhLRAtVn7yty5LPFCg" className="youtube"><img alt="" src='https://i.ibb.co/dWTdwMS/Grupo-25.png' /></a>
-                <a href="https://www.instagram.com/myconexion/" className="instagram"><img alt="" src='https://i.ibb.co/1MrLW7G/Grupo-16.png' /></a>
+                <button onClick={() => window.open('https://wa.me/5215529018021?text=Bienvenido%20a%20Conexión%20y%20Consciencia,%20Terapia%20con%20Ángeles,%20en%20que%20puedo%20ayudarte?', "_blank")} className="whatsapp"><WaImg alt="" src='https://i.ibb.co/7g13zC5/whatsapp.png' /></button>
+                <button onClick={() => window.open('https://www.facebook.com/conexionyconsciencia', "_blank")} className="facebook"><img alt="" src='https://i.ibb.co/MNMxD7M/facebook.png' /></button>
+                <button onClick={() => window.open('https://www.youtube.com/channel/UCFLEejhLRAtVn7yty5LPFCg', "_blank")} className="instagram"><img alt="" src='https://i.ibb.co/dWTdwMS/Grupo-25.png' /></button>
+                <button onClick={() => window.open('https://www.instagram.com/myconexion/', "_blank")} className="google"><img alt="" src='https://i.ibb.co/1MrLW7G/Grupo-16.png' /></button>
+
             </div>
 
 
--- src/components/Social.js
+++ src/components/Social.js
@@ -4,10 +4,10 @@ import '../css/Social.css';
 function Social() {
     return (
         <div className='social'>
-            <a href="https://wa.me/5215529018021?text=Bienvenido%20a%20Conexión%20y%20Consciencia,%20Terapia%20con%20Ángeles,%20en%20que%20puedo%20ayudarte?" className="whatsapp"><img alt="" src='https://i.ibb.co/7g13zC5/whatsapp.png'/></a>
-            <a href="https://www.facebook.com/conexionyconsciencia" className="facebook"><img alt="" src='https://i.ibb.co/dcxGzMw/facebook.png'/></a>
-            <a href="https://www.instagram.com/myconexion/" className="twitter"><img alt="" src='https://i.ibb.co/pfn4cc7/Grupo-22.png'/></a>
-            <a href="https://www.youtube.com/channel/UCFLEejhLRAtVn7yty5LPFCg" className="google"><img alt="" src='https://i.ibb.co/yXnTjDT/youtube.png'/></a>
+            <button onClick={() =>window.open('https://wa.me/5215529018021?text=Bienvenido%20a%20Conexión%20y%20Consciencia,%20Terapia%20con%20Ángeles,%20en%20que%20puedo%20ayudarte?',"_blank")} className="whatsapp"><img alt="" src='https://i.ibb.co/7g13zC5/whatsapp.png'/></button>
+            <button onClick={() =>window.open('https://www.facebook.com/conexionyconsciencia',"_blank")}  className="facebook"><img alt="" src='https://i.ibb.co/dcxGzMw/facebook.png'/></button>
+            <button onClick={() =>window.open('https://www.instagram.com/myconexion/',"_blank")}  className="twitter"><img alt="" src='https://i.ibb.co/pfn4cc7/Grupo-22.png'/></button>
+            <button onClick={() =>window.open('https://www.youtube.com/channel/UCFLEejhLRAtVn7yty5LPFCg',"_blank")} className="google"><img alt="" src='https://i.ibb.co/yXnTjDT/youtube.png'/></button>
         </div>
     )
 }
